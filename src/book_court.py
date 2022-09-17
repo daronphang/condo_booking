cd0600103db42fadbedd9cfb0e2409f407e9c582
daronphang <daronphang@gmail.com>
2022-09-17 10:51:27 +0800

    added book court

--- src/book_court.py
+++ src/book_court.py
@@ -1,5 +1,7 @@
 import os
 import logging
+import requests
+import datetime
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 from utils import GetConnSession,PostConnSession
@@ -14,9 +16,8 @@ class BookCourt:
     PASSWORD = os.environ.get('PASSWORD')
 
     def __init__(self):
-        self._cookies = None
-        self._application_cookie = None
-        self.csrf_token = None
+        self.jar = requests.cookies.RequestsCookieJar()
+        self._csrf_token = None
 
     def get_token(self):
         config = {
@@ -29,39 +30,81 @@ class BookCourt:
             'COOKIES': None,
             'HEADERS': None
         }
-        with GetConnSession(config) as s:
-            self._cookies = s.cookies
-            soup = BeautifulSoup(s.text, features='html.parser')
-            self.csrf_token = soup.find('input', {'name': '__RequestVerificationToken'}).get('value')
+        with GetConnSession(config) as r:
+            if r.status_code >= 400:
+                raise Exception('unable to get token')
+            self.jar.update(r.cookies)
+            soup = BeautifulSoup(r.text, features='html.parser')
+            self._csrf_token = soup.find('input', {'name': '__RequestVerificationToken'}).get('value')
+
             
-
-
     def login(self):
         config = {
             'URL': 'https://grandeur8.net/account/login',
             'DATA': {
-                '__RequestVerificationToken': self.csrf_token,
+                '__RequestVerificationToken': self._csrf_token,
                 'Username': self.EMAIL,
                 'Password': self.PASSWORD,
                 'RememberMe': False
             },
             'PAYLOAD': None,
             'PARAMS': None,
-            'REDIRECTS': True,
+            'REDIRECTS': False,
             'VERIFY': True,
-            'COOKIES': self._cookies,
+            'COOKIES': self.jar,
             'HEADERS': None
         }
-        with PostConnSession(config) as s:
-            logger.info(s.cookies)
-            print(s.url)
-            print(s.status_code)
-            # self._application_cookie = s.cookies['.AspNet.ApplicationCookie']
-            # logger.info(self._application_cookie)
+        with PostConnSession(config) as r:
+            if r.status_code >= 400:
+                raise Exception('unable to login')
+            self.jar.update(r.cookies)
+
+        
+    def book_court(self):
+        next_week_date = datetime.date.today() + datetime.timedelta(days=6)
+        book_date = f'{next_week_date.strftime("%Y-%m-%d")}T3:00:00.000Z'
+
+        print('bookdate', book_date)
+
+        config = {
+            'URL': 'https://grandeur8.net/facilities/booking',
+            'DATA': {
+                'FacilityId': 92,
+                'FacilityTypeId': 1041,
+                'BookDate': book_date,
+                'BookHours': 1,
+                'UnitId': 0,
+                'UserId': 0,
+                'Peak': False,
+                'DropDown1': None,
+            },
+            'PAYLOAD': None,
+            'PARAMS': None,
+            'REDIRECTS': False,
+            'VERIFY': True,
+            'COOKIES': self.jar,
+            'HEADERS': None,
+        }
+        with PostConnSession(config) as r:
+            if r.status_code >= 400:
+                raise Exception('unable to book court')
+            print(r.text)
     
     def execute(self):
         self.get_token()
         self.login()
+        self.book_court()
 
-        
 
+'''
+FacilityId: 92
+FacilityTypeId: 1041
+BookDate: 2022-09-19T03:00:00.000Z
+BookHours: 1
+UnitId: 0
+UserId: 0
+Peak: false
+DropDown1: 
+'''      
+
+# {"Heading":"Booking Successful","Type":"success","Body":"Your booking has been reserved. An email confirming your booking has been sent to <strong>patricialim38@hotmail.com</strong>. Please ensure that you have <strong>read the booking rules and paid any necessary fees</strong> before using the facility. You are able to print a receipt of your booking (if required) via the booking list page <a href=/facilities/booking>here</a>","Dismissable":false},"result":{"Email":"patricialim38@hotmail.com","BookingId":896502}
